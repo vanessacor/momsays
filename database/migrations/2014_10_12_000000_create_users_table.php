b72dda55802ede8dbefac4f1eda03107687239de
vanessa <vanessapcbarros@gmail.com>
2020-12-23 12:50:34 +0100

    add roles and points column to user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('role')->default('child');
+            $table->integer('points')->default(0);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
