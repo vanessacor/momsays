9ef26e23e18ae652890f1b6a8e9f83a685100ddf
vanessa <vanessapcbarros@gmail.com>
2020-12-27 11:22:43 +0100

    style: change style on login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="title">
+    <h2>Welcome to Mom Says</h2>
+</div>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+            <div class="login card">
+                <div class=" login-title card-header">
+                    <h3>Login</h3>
+                </div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
@@ -26,8 +31,16 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
+                            <div class="login-password col-md-4">
+                                <label for="password" class="col-form-label text-md-right">{{ __('Password') }}</label>
+                                @if (Route::has('password.request'))
+                                <a class="col-form-label text-md-left" href="{{ route('password.request') }}">
+                                    {{ __('Forgot Your Password?') }}
+                                </a>
+                                @endif
+
+                            </div>
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
@@ -51,25 +64,19 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+                        <div class="login-buttons form-group">
+                            <button type="submit" class="btn">
+                                {{ __('Login') }}
+                            </button>
 
-                                @if (Route::has('password.request'))
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                                @endif
-                            </div>
+
                         </div>
                     </form>
                     <div class="register">
                         <p>New User?</p>
                         @if (Route::has('register'))
 
-                        <a class="nav-link" href="{{ route('register') }}">SignUP</a>
+                        <a href="{{ route('register') }}">SignUP</a>
 
                         @endif
                     </div>
